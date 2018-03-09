397532922ec7ae4b9980d91657ad10a4a41fa2d2
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-09 16:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ function removeLastKitten(name) {
   return kittens.slice(name)
 }
 
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+function removeFirstKitten() {
+  return kittens.slice()
 }
